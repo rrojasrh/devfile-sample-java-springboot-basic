9066917049a0ddc1dc910f121b10780bb5598076
Raúl Rojas <rrojas@redhat.com>
2024-10-24 16:08:48 -0600

    Prueba

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello world! Demo para TransUnion";
+        return "Otro mensaje!!! Por la anecdota";
     }
 
     public static void main(String[] args) {
