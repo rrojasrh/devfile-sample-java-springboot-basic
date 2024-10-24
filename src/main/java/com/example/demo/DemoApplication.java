e14470c50170d9b9085159ee76516d43a56a2f11
Raúl Rojas <rrojas@redhat.com>
2024-10-24 14:51:50 -0600

    Modificación al mensaje de respuesta

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello world! Demo para TransUnion";
     }
 
     public static void main(String[] args) {
